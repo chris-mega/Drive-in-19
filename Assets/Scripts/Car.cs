b08e54dd7d87150b4fb2889924b725469d70f394
Christian Melendez <32151767+chris-mega@users.noreply.github.com>
2020-12-16 23:04:19 -0600

    first game files

---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    [SerializeField] private float speed = 2f;
+    private float init_y = 8f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(4, 0, -6);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.left * speed * Time.deltaTime);
+
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] private float waitTimeSpawnCar = 5;
+    [SerializeField] private bool playerAlive = true;
+    [SerializeField] private bool queueFull = false;
+    [SerializeField]
+    private GameObject[] carTypes;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnCarRoutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnCarRoutine()
+    {
+        while (playerAlive && !queueFull)
+        {
+            GameObject newCar = Instantiate(carTypes[Random.Range(0, carTypes.Length)]);
+            yield return new WaitForSeconds(waitTimeSpawnCar);
+        }
+    }
+}
